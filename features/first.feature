70ebf9b6c53bb97b08d6b60ac479295f63621046
Alyssa Lehmann <lehmann.a.m78654@tamu.edu>
2018-02-27 19:01:36 +0000

    forced to commit

--- features/first.feature
+++ features/first.feature
@@ -1,10 +1,9 @@
 #encoding: utf-8
-
-Feature: Showcase the simplest possible cucumber scenario
+Feature: Showcase the simplest possible Cucumber scenario
   In order to verify that cucumber is installed and configured correctly
   As an aspiring BDD fanatic 
   I should be able to run this scenario and see that the steps pass (green like a cuke)
-
+ 
   Scenario: Cutting vegetables
     Given a cucumber that is 30 cm long
     When I cut it in halves
--- features/step_definitions/first_steps.rb
+++ features/step_definitions/first_steps.rb
@@ -1,23 +1,20 @@
 #encoding: utf-8
-
-Given /^a cucumber that is (\d+) cm long$/ do |length|
+Given /^a cucumber that is (\d+) cm long$/ do |arg1|
   @cucumber = {:color => 'green', :length => length.to_i}
 end
-
-When /^I (?:cut|chop) (?:it|the cucumber) in (?:halves|half|two)$/ do
-  @choppedCucumbers = [  
-    {:color => @cucumber[:color], :length => @cucumber[:length] / 2},
-    {:color => @cucumber[:color], :length => @cucumber[:length] / 2}
-  ]
+ 
+When /^I cut it in halves$/ do
+  @allCucumbers = [{:color => @cucumber[:color], :length => @cucumber[:length]/2}, 
+                    {:color => @cucumber[:color], :length => @cucumber[:length]/2}
+                ]
 end
-
+ 
 Then /^I have two cucumbers$/ do
-  expect(@choppedCucumbers.length).to eq(2)
+  @allCucumbers.length.should == 4
 end
-
-Then /^both are (\d+) cm long$/ do |length|
-  @choppedCucumbers.each do |cuke|
-    expect(cuke[:length]).to eq(length.to_i)
+ 
+Then /^both are (\d+) cm long$/ do |arg1|
+  @allCucumbers.each do |cucum|
+      cucum[:length].should == arg1.to_i
   end
 end
-
